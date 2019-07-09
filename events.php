1a1a6b04cee8c97faecd254894b9303e14eee396
antonio <prodoson@gmail.com>
2019-07-09 16:54:26 -0400

    Initial commit

---
+++ events.php
@@ -0,0 +1,311 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 6/17/2019
+ * Time: 5:31 PM
+ */
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90995944-4"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-90995944-4');
+    </script>
+
+    <meta charset="UTF-8">
+    <title>Mi Alma Restaurant</title>
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1, user-scalable = no"/>
+
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+    <link href="css/events.css" rel="stylesheet">
+    <link href="css/mobile.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/v4-shims.css">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
+
+
+
+</head>
+<body>
+<!-- Header -->
+
+
+<?php include("views/nav_pages.html")?>
+<br>
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" style="top:-25px;">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="img/Events/Concert-15.jpg" alt="Second slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Concert-16.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption d-md-block d-flex justify-content-center "  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Concert-29.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center" id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Concert-29.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Concert-31.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Optimo%20Concert-24.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Optimo%20Concert-25.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Optimo%20Concert-26.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Optimo%20Concert-27.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Events/Optimo%20Concert-28.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption  d-md-block d-flex justify-content-center"  id="hero">
+
+                <center>
+                    <div class="eventHeaderBox">
+                        <h1 class="eventHeaderText">Events</h1>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+    <div class="arrowRemove">
+        <a class="carousel-control-prev .d-none .d-md-block" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon .d-none .d-md-block" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+<div class="container">
+    <center>
+        <img src="img/logo.png" style="height:200px;width:300px;object-fit: contain;">
+    </center>
+
+    <hr style="margin-bottom:0px;">
+    <div class="col-md-6"></div>
+    <div class="row" style="display: inline;padding-top: -10px;" >
+        <h1 style="text-align: center; color:#1b1b1b;margin-bottom: 3px;">Our upcoming <strong>Events</strong></h1>
+        <h5 class="" style="text-align: center;font-size: 17px;">Celebrate your next special occasion with us</h5>
+        <div class="section-title-divider" style="width:300px;"></div>
+    </div>
+    <div class="row" id="videoRow" style="display: inline; padding-top: -10px;">
+        <center>
+            <video id="video" style="margin-bottom: 30px" controls>
+                <source src="vid/event_video2.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+            <!--img src="img/logo.png" style="padding-bottom: 200px;" class="eventHeaderImage""-->
+        </center>
+    </div>
+
+    <div class="row" style="display:inline;">
+        <div class="card-deck" style="width:80%;margin:auto;">
+            <div class="col-md-3"></div>
+            <div class="card" style="border-color: deepskyblue">
+                <img class="card-img-top" src="img/Events/event3.jpeg" alt="Card image cap" style="height:400px;object-fit: contain;">
+                <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
+                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">El Varon De La Bachata</h5>
+                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">Music by DJ Travieso
+                        <br><br>
+                        <strong>Date: June 28th</strong><br>
+                        <strong>Tickets: $30</strong><br>
+                        <strong>Start: 9pm</strong><br>
+                        <strong>End: 1am</strong><br>
+                    </p>
+
+                </div>
+                <div id="content-desktop" class="card-footer" style="background-color: deepskyblue;height:50px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+
+                <div id="content-mobile" class="card-footer" style="background-color: deepskyblue;height:60px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+
+        </div>
+    </div>
+    <div class="col-md-6"></div>
+
+
+</div>
+    <div class="col-md-6"></div>
+</div>
+
+
+<div class="container">
+
+    <hr style="margin-bottom:0px;">
+    <div class="col-md-6"></div>
+    <div class="row" style="display: inline;padding-top: -10px;" >
+        <h1 style="text-align: center; color:#1b1b1b;margin-bottom: 3px;">Our past <strong>Events</strong></h1>
+        <div class="section-title-divider" style="width:300px;"></div>
+    </div>
+    <!--<div class="row" id="videoRow" style="display: inline; padding-top: -10px;">
+        <center>
+            <video id="video" style="margin-bottom: 30px" controls>
+                <source src="vid/event_video.MP4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </center>
+    </div>-->
+    <div class="row" style="display:inline;">
+        <div class="card-deck" style="width:80%;margin:auto;">
+            <div class="card" style="border-color: deepskyblue">
+                <img class="card-img-top" src="img/Events/event1.JPG" alt="Card image cap" style="height:400px;object-fit: contain;">
+                <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
+                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">Miss Dominican Republic 2019</h5>
+                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">The Coronation Ceremony of Miss Dominican Republic of Rhode Island 2019 & the presentation of Carnavalito by Emerson Brito
+                        <br><br>
+                        <strong>Date: June 23rd</strong><br>
+                        <strong>Tickets: $15</strong><br>
+                        <strong>Start: 4pm</strong><br>
+                    <small class=" d-flex justify-content-center">Formal Attire Preferred</small>
+                    </p>
+
+                </div>
+                <div id="content-desktop" class="card-footer" style="background-color: deepskyblue;height:50px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+
+                <div id="content-mobile" class="card-footer" style="background-color: deepskyblue;height:60px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+            </div>
+            <div class="card" style="border-color: deepskyblue">
+                <img class="card-img-top" src="img/Events/event2.JPG" alt="Card image cap" style="height:400px;object-fit: contain;">
+                <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
+                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">Miss Dominican Republic 2019</h5>
+                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">The Coronation Ceremony of Miss Dominican Republic of Rhode Island 2019 & the presentation of Carnavalito by Emerson Brito
+                        <br><br>
+                        <strong>Date: June 23rd</strong><br>
+                        <strong>Tickets: $15</strong><br>
+                        <strong>Start: 4pm</strong><br>
+                        <small class=" d-flex justify-content-center">Formal Attire Preferred</small>
+                    </p>
+
+                </div>
+                <div id="content-desktop" class="card-footer" style="background-color: deepskyblue;height:50px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+
+                <div id="content-mobile" class="card-footer" style="background-color: deepskyblue;height:60px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6"></div>
+</div>
+
+
+<br id="content-desktop">
+<?php include("views/footer.html")?>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
