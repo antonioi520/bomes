07aab7bc29ca034e9d1325381bd15797aecce704
Luciano728 <lpm72896@gmail.com>
2019-08-09 14:09:57 -0400

    uploaded and embedded event video 3 and 4 to youtube, added to gallery page, sized to fit all sized devices in chrome test environment

--- gallery.php
+++ gallery.php
@@ -3083,21 +3083,31 @@
             <div class="videoSlides">
                 <div class="numbertext">3 / 4</div>
                 <!--video controls id="galleryVid" src="vid/event_video2.mp4" title="El Varon De La Bachata" style="width:100%"></video-->
-                <video id="galleryVid" title="Don Miguelo Concert" style="width:100%" controls>
+                <!--video id="galleryVid" title="Don Miguelo Concert" style="width:100%" controls>
                     <source src="vid/event_video3.mp4" type="video/mp4">
                     Your browser does not support the video tag.
-                </video>
-                <p><center style="color: white;">Don Miguelo Concert</center></p>
+                </video-->
+                <center>
+                    <iframe id="galleryVid" width="67%" height="430px" src="https://www.youtube.com/embed/Spb4DWDp9ts?rel=0&amp;showinfo=0"
+                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                    </iframe>
+                </center>
+                <center style="color: white;">Don Miguelo Concert</center>
             </div>
 
             <div class="videoSlides">
                 <div class="numbertext">4 / 4</div>
                 <!--video controls id="galleryVid" src="vid/event_video2.mp4" title="El Varon De La Bachata" style="width:100%"></video-->
-                <video id="galleryVid" title="Adolescentes Concert" style="width:100%" controls>
+                <!--video id="galleryVid" title="Adolescentes Concert" style="width:100%" controls>
                     <source src="vid/event_video4.mp4" type="video/mp4">
                     Your browser does not support the video tag.
-                </video>
-                <p><center style="color: white;">Adolescentes Concert</center></p>
+                </video-->
+                <center>
+                    <iframe id="galleryVid" width="67%" height="430px" src="https://www.youtube.com/embed/uAkRHBR4FzY?rel=0&amp;showinfo=0"
+                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                    </iframe>
+                </center>
+                <center style="color: white;">Adolescentes Concert</center>
             </div>
 
             <a class="prev" onclick="plusVideoSlides(-1)">&#10094;</a>
